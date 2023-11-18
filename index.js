dbdf3a1592cd621effcf3449e901e2061407738b
mrkocka <radics.pepe@gmail.com>
2023-11-18 23:01:37 +0100

    majdnem jó

--- index.js
+++ index.js
@@ -16,43 +16,48 @@ server.listen(port, listeningStartedCallback);
 let gyumolcskosar = [
   {
     id: 1,
-    name: "Alma",
+    name: "alma",
     image: "./img/alma.png",
   },
   {
     id: 2,
-    name: "Dinnye",
+    name: "dinnye",
     image: "./img/dinnye.png",
   },
   {
     id: 3,
-    name: "Eper",
+    name: "eper",
     image: "./img/eper.png",
   },
   {
     id: 4,
-    name: "Narancs",
+    name: "narancs",
     image: "./img/narancs.png",
   },
   {
     id: 5,
-    name: "Szőlő",
+    name: "szőlő",
     image: "./img/szolo.png",
   },
   {
     id: 6,
-    name: "Ananasz",
+    name: "ananasz",
     image: "./img/ananasz.png",
   },
   {
     id: 7,
-    name: "Körte",
+    name: "körte",
     image: "./img/korte.png",
   },
   {
     id: 8,
-    name: "Barack",
+    name: "barack",
     image: "./img/barack.png",
+  },
+  {
+    id: 100,
+    name: "hiba",
+    image: "./img/nincs.png",
   },
 ];
 
@@ -61,12 +66,14 @@ server.get("/api/gyumolcskosar", (req, res) => {
   res.send(gyumolcskosar);
 });
 
-server.get("/api/gyumolcskosar/:id", (req, res) => {
-  const id = Number.parseInt(req.params.id, 10);
-  console.log(`Volt egy kérés a ${id}-re`);
-  const gyumi = gyumolcskosar.find((gyumi) => gyumi.id === id);
-  if (typeof gyumi === "undefined") {
-    res.status(404).sendFile(path.join(__dirname, "./public/hiba.html"));
+server.get("/api/gyumolcskosar/:name", (req, res) => {
+  const name = req.params.name.toLowerCase();
+  console.log(`Volt egy kérés erre: ${name}-re`);
+  const gyumi = gyumolcskosar.find((gyumi) => gyumi.name === name);
+
+  if (!gyumi) {
+    // Ha nincs találat, küldje vissza a "nincs.png" képet
+    res.send({ image: "./img/nincs.png" });
   } else {
     res.send(gyumi);
   }
--- public/main.js
+++ public/main.js
@@ -1,20 +1,28 @@
 let selectBox = document.getElementById("selectBox");
 let button = document.getElementById("valastButton-js");
 let imggyumi = document.querySelector(".gyumiImg-js");
+let txt = document.querySelector(".gyumi-txt");
 
 button.addEventListener("click", valuegyum);
 
 function valuegyum() {
   let selectedValue = document.getElementById("wor-input").value;
-  let ertek = selectedValue;
+  let ertek = selectedValue.toLowerCase();
   console.log(ertek);
 
-  /* console.log(`http://localhost:666/api/gyumolcskosar/${ertek}`);
+  console.log(`http://localhost:666/api/gyumolcskosar/${ertek}`);
   fetch(`http://localhost:666/api/gyumolcskosar/${ertek}`)
     .then((json) => json.json())
     .then((objektum) => {
-      //Kép
-      imggyumi.src = objektum.image;
-      imggyumi.width = "50px";
-    }); */
+      if (objektum.image) {
+        //Ha van kép
+        imggyumi.src = objektum.image;
+        imggyumi.width = "50px";
+      } else {
+        //Ha nincs kép
+        imggyumi.src = "http://localhost:666/public/img/nincs.png";
+        imggyumi.width = "50px";
+        txt.classList.remove("invisible");
+      }
+    });
 }
