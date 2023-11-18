7c5a3361b548974fe8d380f1659615c451518ae5
mrkocka <radics.pepe@gmail.com>
2023-11-18 19:42:43 +0100

    elso változtatás

--- public/main.js
+++ public/main.js
@@ -5,15 +5,16 @@ let imggyumi = document.querySelector(".gyumiImg-js");
 button.addEventListener("click", valuegyum);
 
 function valuegyum() {
-  let selectedValue = selectBox.options[selectBox.selectedIndex].value;
+  let selectedValue = document.getElementById("wor-input").value;
   let ertek = selectedValue;
   console.log(ertek);
-  console.log(`http://localhost:666/api/gyumolcskosar/${ertek}`);
+
+  /* console.log(`http://localhost:666/api/gyumolcskosar/${ertek}`);
   fetch(`http://localhost:666/api/gyumolcskosar/${ertek}`)
     .then((json) => json.json())
     .then((objektum) => {
       //Kép
       imggyumi.src = objektum.image;
       imggyumi.width = "50px";
-    });
+    }); */
 }
